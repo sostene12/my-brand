e9bdf4c9fff01e742bf485e1d099e910912fb6ec
ngarukiyimana sostene <ngarukiyimanasostene@gmail.com>
2022-12-07 12:15:48 +0200

    responsiveness

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,14 @@
-console.log("hello")
+window.onscroll = function(event){
+    let navbar = document.getElementsByClassName("nav-container");
+    if((window.innerHeight + window.scrollY) > window.innerHeight){
+        navbar[0].classList.add("scroll");
+        console.log(navbar.classList)
+    }
+    else{
+        navbar[0].classList.remove('scroll')
+    }
+};
+
+
+const bars = document.querySelector('.burger');
+
