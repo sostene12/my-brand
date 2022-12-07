64605e0b5671b0245fa74daf8bdfdd640b6616d9
ngarukiyimana sostene <ngarukiyimanasostene@gmail.com>
2022-12-07 12:50:27 +0200

    active navigation

--- scripts/app.js
+++ scripts/app.js
@@ -2,12 +2,21 @@ window.onscroll = function(event){
     let navbar = document.getElementsByClassName("nav-container");
     if((window.innerHeight + window.scrollY) > window.innerHeight){
         navbar[0].classList.add("scroll");
-        console.log(navbar.classList)
     }
     else{
         navbar[0].classList.remove('scroll')
     }
 };
+// active navigation
+
+const navItems = document.querySelectorAll('.nav-items a');
+navItems.forEach(item =>{
+    item.addEventListener('click',function(event){
+        const activebtn = document.querySelector('.active');
+        activebtn.classList.remove('active');
+        event.target.classList.add('active');
+    })
+});
 
 
 const bars = document.querySelector('.burger');
