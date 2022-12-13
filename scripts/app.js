2cd74c2ef7695768995a5fc72e835fe9f84221c9
ngarukiyimana sostene <ngarukiyimanasostene@gmail.com>
2022-12-13 14:13:19 +0200

    form validation of login and signup as well as contact forms

--- scripts/app.js
+++ scripts/app.js
@@ -41,24 +41,104 @@ bars.addEventListener('click',(e) =>{
     const navLinks = document.querySelector('.nav-links');
     const navContents = document.querySelector('.nav-contents');
     navContents.appendChild(navLinks)
+    navContents.style.display = "block"
     navContents.classList.toggle('open')
     bars.classList.toggle('open')
 });
 
 // tabs
-const tab = document.querySelector('.accounts');
 const tabHeader = document.querySelector('.tab-header');
 const tabHeaderElements = document.querySelectorAll('.tab-header > div');
 const tabBody = document.querySelector('.tab-body');
-const tabBodyElements = document.querySelectorAll('.tab-Body > div');
+const tabBodyElements = document.querySelectorAll('.tab-body > div');
 
 
 for(let i=0;i<tabHeaderElements.length;i++){
-    tabHeaderElements[i].addEventListener('click',() =>{
+    tabHeaderElements[i].addEventListener('click',(e) =>{
        tabHeader.querySelector('.active').classList.remove('active');
         tabHeaderElements[i].classList.add('active');
+        // tabBodyElements[i]
         tabBody.querySelector('.active').classList.remove('active');
-        tabBodyElements[i].classList.add('active')
+        console.log(tabBodyElements[i]);
+        tabBodyElements[i].classList.add('active');
     });
 }
+
+const nameRegex = new RegExp('/[a-zA-Z]/g');
+const passwordRegex= new RegExp("/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/");
+const emailRegex = new RegExp("/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/");
+
+
+// contact Form validation
+const contactForm = document.querySelector("#contact-form");
+const nameError = document.querySelector('.nameError');
+const emailError = document.querySelector('.emailError');
+const messageError = document.querySelector('.messageError')
+contactForm.addEventListener('submit',(e) =>{
+    e.preventDefault();
+    console.log(contactForm)
+    const name=contactForm.name.value;
+    const email = contactForm.email.value;
+    const message = contactForm.querySelector('.message').value;
+    const nameTest = name.search(/[a-zA-Z]/g);
+    // name validation
+    if (nameTest == -1||name == ""){
+        nameError.innerHTML = 'Please enter only letters in this field.';
+    }
+    else{
+        nameError.style.visibility="hidden";
+    }
+
+    // emaill validation
+    if(!email.match(emailRegex) || email==""){
+        emailError.innerHTML = "please enter the email";
+    }
+    else{
+        emailError.style.visibility = "hidden"
+    }
+
+    if(message==""){
+        messageError.innerHTML = "please enter the goals";
+    }
+})
+
+
+// signup-form validation
+const signupForm = document.querySelector('.signup-form');
+signupForm.addEventListener('submit',(e) =>{
+    e.preventDefault();
+    const name = signupForm.username.value;
+    const email = signupForm.email.value;
+    const password = signupForm.password.value;
+    if(name=='' || password==''){
+        alert("please fill all fields");
+    }
+    if(!emailRegex.test(email)){
+        alert("enter valid email")
+    }
+    if(!passwordRegex.test(password)){
+        alert("password is invalid")
+    }
+    if(!nameRegex.test(name)){
+        alert("please enter valid name")
+    }
+});
+
+
+// login-form validation
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener('submit',(e) =>{
+    e.preventDefault();
+    const name = loginForm.username.value;
+    const password = loginForm.password.value;
+    if(name=='' || password==''){
+        alert("please fill all fields");
+    }
+    if(!passwordRegex.test(password)){
+        alert("password is invalid")
+    }
+    if(!nameRegex.test(name)){
+        alert("please enter valid name")
+    }
+})
 
